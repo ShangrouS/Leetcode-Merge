eda34928f38e0869a7a69e1309cf0483dd759c4e
ShangrouS <35820840+ShangrouS@users.noreply.github.com>
2018-02-10 12:32:02 -0800

    Create 291. Word Pattern II.java

---
+++ 291. Word Pattern II.java
@@ -0,0 +1,51 @@
+题意：
+   给定一个字符出现顺序的pattern, 判断其对应的str是不是遵守一致的字符或者单词出现模式（x,y双向映射）
+   139. Word Break 和 290. Word Pattern I 的综合题
+
+思路：
+   backtracking题，结合了HashMap和HashSet。
+   1，从str的取出单词，判断 单词分割的位置在哪里, 用字符长度逐一扫遍所有的字符。
+      => 相当于 hashset 逐一存储出现的字符：r re red
+   2，判断映射（isMatch）是否成立 -> 若建立了映射，就把 pattern对应的 (char) c  和 (str) word 加入 hashmap.
+   3, 注意边界条件 -> 判断str 和 pattern，对应映射长度相等
+
+复杂度：
+     time : O(2^n) 
+     space : O(n)
+  
+public class WordPatternII {
+         public boolean wordPatternMatch(String pattern, String str) {
+        HashMap<Character, String> map = new HashMap<>();
+        HashSet<String> set = new HashSet<>();
+        return isMatch(str, 0, pattern, 0, map, set);
+    }
+
+    private boolean isMatch(String str, int i, String pat, int j, HashMap<Character, String> map, HashSet<String> set) {
+        if (i == str.length() && j == pat.length()) return true;
+        if (i == str.length() || j == pat.length()) return false;
+
+        char c = pat.charAt(j);
+        if (map.containsKey(c)) {
+            String s = map.get(c);
+            if (!str.startsWith(s, i)) {
+                return false;
+            }
+            return isMatch(str, i + s.length(), pat, j + 1, map, set);
+        }
+
+        for (int k = i; k < str.length(); k++) {
+            String p = str.substring(i, k + 1);
+            if (set.contains(p)) {
+                continue;
+            }
+            map.put(c, p);
+            set.add(p);
+            if (isMatch(str, k + 1, pat, j + 1, map, set)) {
+                return true;
+            }
+            map.remove(c);
+            set.remove(p);
+        }
+        return false;
+    }
+}
